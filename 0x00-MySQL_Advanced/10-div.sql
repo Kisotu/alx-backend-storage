0bdb8e98b3aae474e7c864d8e76f1be824abce57
Oloishorua <jacoboloishorua@gmail.com>
2024-08-09 00:18:12 +0300

    task 10 -  script that creates a function SafeDiv that divides (and returns) the first by the second number

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,14 @@
+-- script that creates a function SafeDiv
+-- that divides (and returns) the first by the second number
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+    DECLARE result FLOAT DEFAULT 0;
+
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
+DELIMITER ;
