a58ed11ac6012efea912563a2d349dec02986e54
Oloishorua <jacoboloishorua@gmail.com>
2024-08-13 23:32:31 +0300

    task 10 - changes topics of a doc based on name

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function that changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+	"""chanes topics of a doc based on the name
+	"""
+	mongo_collection.update_many(
+		{'name': name},
+		{'$set': {'topics': topics}}
+	)
+	
