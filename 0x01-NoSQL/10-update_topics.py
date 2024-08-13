62c7c2afc7b84878bfbd51ffd53c4d8720f2d187
Oloishorua <jacoboloishorua@gmail.com>
2024-08-13 23:34:09 +0300

    task 10 - changes topics of a doc based on name

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,10 +3,7 @@
 
 
 def update_topics(mongo_collection, name, topics):
-	"""chanes topics of a doc based on the name
-	"""
+	"""chanes topics of a doc based on the name"""
 	mongo_collection.update_many(
 		{'name': name},
-		{'$set': {'topics': topics}}
-	)
-	
+		{'$set': {'topics': topics}})
