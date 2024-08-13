73491d05d8803a3a31a8f8f633ff266463d6dbc5
Oloishorua <jacoboloishorua@gmail.com>
2024-08-13 23:17:47 +0300

    task 9 - inserts a doc based on kwargs

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function that inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+	"""inserts a doc based on kwargs
+	"""
+	result = mongo_collection.insert_one(kwargs)
+	return result.inserted_id
