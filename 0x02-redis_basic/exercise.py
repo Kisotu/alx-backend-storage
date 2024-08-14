49ffbb6225d74cf029f73c4d8002a4da72a16776
Oloishorua <jacoboloishorua@gmail.com>
2024-08-15 00:29:58 +0300

    task 4 - displays the call history of  a particular function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -38,6 +38,31 @@ def call_history(method: Callable) -> Callable:
     return invoke
 
 
+def replay(fn: Callable) -> None:
+    '''Displays the call history of a particular function
+    '''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    func_name = fn.__qualname__
+    in_key = '{}:inputs'.format(func_name)
+    out_key = '{}:outputs'.format(func_name)
+    func_call_count = 0
+    if redis_store.exists(func_name) != 0:
+        func_call_count = int(redis_store.get(func_name))
+    print('{} was called {} times:'.format(func_name, func_call_count))
+    func_inputs = redis_store.lrange(in_key, 0, -1)
+    func_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(func_inputs, func_outputs):
+        print('{}(*{}) -> {}'.format(
+            func_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
+
+
 class Cache:
     '''An object for storing data in a Redis data storage.
     '''
